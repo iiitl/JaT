11a1871771fef387f7de4363c418f4d26ce7e0f4
flyingdelta <flyingdelta@outlook.com>
2022-10-03 21:20:59 +0530

    synchronous chat - Client and Server classes

--- Client.java
+++ Client.java
@@ -1,36 +1,92 @@
-import java.io.*;
-import java.util.*;
 import java.net.*;
+import java.io.*;
 
-public class Client {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Client implements Runnable {
+    String hostName;
+    int portNumber;
+    Socket client;
+    BufferedReader in;
+    PrintWriter out;
+    boolean exit;
+    String endCodeWord = "OVER AND OUT";
+
+    public Client(String hostName, int portNumber) {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+        this.exit = false;
+    }
+
+    public void run() {
         try {
-            Socket s = new Socket("localhost", 8000);
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String endCodeWord = "OVER AND OUT";
-            System.out.println("This chat system is like a walkie talkie, you (client) and the server get turns to send a 1 line message at a time. Send \"" + endCodeWord + "\" to end the session anytime!");
-            while(true) {
-                System.out.print("From Client (you): ");
-                String outgoing = sc.nextLine();
-                dos.writeUTF(outgoing);
-                dos.flush();
-                if(outgoing.equals(endCodeWord)) {
-                    System.out.println("Ending session...");
-                    break;
-                }
-                String incoming = dis.readUTF();
-                if(incoming.equals(endCodeWord)) {
-                    System.out.println("The server is ending the session...");
-                    break;
-                }
-                System.out.println("From Server: " + incoming);
+            client = new Socket(hostName, portNumber);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+
+            MessageHandler incomingHandler = new MessageHandler();
+            Thread t = new Thread(incomingHandler);
+            t.start();
+
+            String incomingMessage;
+            while ((incomingMessage = in.readLine()) != null) {
+                System.out.println(incomingMessage);
             }
-            dos.close();
-            s.close();            
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            exit();
         }
+    }
+
+    class MessageHandler implements Runnable {
+        public void run() {
+            try {
+                // Take inputs
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String message;
+                // while (!exit && (message = reader.readLine()) != endCodeWord) {
+                // // Send them to the server
+                // out.println(message);
+                // }
+                while (!exit) {
+                    message = reader.readLine();
+                    // To replace the input from the console
+                    System.out.print(String.format("\033[%dA", 1)); // Move up
+                    System.out.print("\033[2K"); // Erase line content
+                    if (message.equals(endCodeWord)) {
+                        reader.close();
+                        exit();
+                    } else {
+                        out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                // TODO: handle exception
+                e.printStackTrace();
+                exit();
+            }
+        }
+    }
+
+    public void exit() {
+        out.println("has disconnected");
+        exit = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            // TODO: handle exception
+            // ignore
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java Client <host name> <port number>");
+            System.exit(1);
+        }
+        Client client = new Client(args[0], Integer.parseInt(args[1]));
+        client.run();
     }
 }
--- Server.java
+++ Server.java
@@ -1,36 +1,157 @@
-import java.io.*;
-import java.util.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.*;
 
-public class Server {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Server implements Runnable {
+
+    ArrayList<ClientHandler> clients;
+    ServerSocket server;
+    int portNumber;
+    boolean exit;
+    ExecutorService pool;
+    // ArrayList<String> randomNames = ["giant giraffe", "curious cuttlefish"];
+
+    public Server(int portNumber) {
+        clients = new ArrayList<>();
+        this.portNumber = portNumber;
+        exit = false;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("The server is listening at " + portNumber);
         try {
-            ServerSocket ss = new ServerSocket(8000);
-            Socket s = ss.accept();
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String endCodeWord = "OVER AND OUT";
-            System.out.println("This chat system is like a walkie talkie, you (server) and the client get turns to send a 1 line message at a time. Send \"" + endCodeWord + "\" to end the session anytime!");
-            while(true) {
-                String incoming = dis.readUTF();
-                if(incoming.equals(endCodeWord)) {
-                    System.out.println("The client is ending the session...");
-                    break;
+            server = new ServerSocket(portNumber);
+            pool = Executors.newCachedThreadPool();
+            while (!exit) {
+                Socket connection = server.accept();
+                ClientHandler client = new ClientHandler(connection);
+                clients.add(client);
+                pool.execute(client);
+            }
+        } catch (IOException e) {
+            // TODO: handle IOException
+            e.printStackTrace();
+            exit();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (ClientHandler client : clients) {
+            if (client != null) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    class ClientHandler implements Runnable {
+        Socket client;
+        BufferedReader in; // Get stream from the socket
+        PrintWriter out; // Send the messages to the socket
+        String endCodeWord = "OVER AND OUT";
+        String username;
+
+        public ClientHandler(Socket client) {
+            this.client = client;
+        }
+
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out = new PrintWriter(client.getOutputStream(), true);
+                out.println(
+                        "This chat system is like discord DM, you and other users get to send messages to each other. Send \""
+                                + endCodeWord + "\" to end the session anytime!\nPlease enter your username: ");
+                String inpName = in.readLine();
+                while (!validNickname(inpName)) {
+                    out.println("Please enter your username: ");
+                    inpName = in.readLine();
                 }
-                System.out.println("From Client: " + incoming);
-                System.out.print("From Server (you): ");
-                String outgoing = sc.nextLine();
-                dos.writeUTF(outgoing);
-                dos.flush();
-                if(outgoing.equals(endCodeWord)) {
-                    System.out.println("Ending session...");
-                    break;
+                username = inpName;
+                System.out.println(username + " has connected");
+                broadcast(username + " has joined the chat!");
+                // The main loop
+                String message;
+                while ((message = in.readLine()) != null) {
+                    // TODO: Implement commands
+                    if (message.startsWith("\\")) {
+                        handleCommands(message);
+                    }
+                    broadcast(username + ": " + message);
+                }
+            } catch (IOException e) {
+                // TODO handle IOException
+                e.printStackTrace();
+                exit();
+            }
+        }
+
+        private void handleCommands(String message) {
+            // Do nothing for now
+            return;
+        }
+
+        public boolean validNickname(String userName) {
+            // Handle usernames
+            // Doesn't start with '\' and not ""
+            if (userName == null || userName.startsWith("\\")) {
+                return false;
+            }
+
+            // Unique
+            for (ClientHandler client : clients) {
+                if (!client.equals(this) && client.username.equals(userName)) {
+                    return false;
                 }
             }
-            ss.close();            
+            return true;
+        }
+
+        public void exit() {
+            broadcast(username + " left the chat");
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+    }
+
+    public void exit() {
+        exit = true;
+        try {
+            if (!server.isClosed()) {
+                server.close();
+            }
         } catch (Exception e) {
+            // can't handle
             e.printStackTrace();
         }
+        // Close all clients
+        for (ClientHandler client : clients) {
+            client.exit();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java Server <port number>");
+            System.exit(1);
+        }
+
+        Server server = new Server(Integer.parseInt(args[0]));
+        server.run();
     }
 }
